8e2606f25feea61a7aef1f2053fcb0a499ebb112
johnpsar <jps200010@gmail.com>
2022-12-09 13:26:32 +0200

    cmake changes

---
+++ ergasia2/build/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/SimulatedAnnealing.cpp.o"
+  "CMakeFiles/a.out.dir/SimulatedAnnealing.cpp.o.d"
+  "CMakeFiles/a.out.dir/incrementing.cpp.o"
+  "CMakeFiles/a.out.dir/incrementing.cpp.o.d"
+  "CMakeFiles/a.out.dir/main.cpp.o"
+  "CMakeFiles/a.out.dir/main.cpp.o.d"
+  "CMakeFiles/a.out.dir/util.cpp.o"
+  "CMakeFiles/a.out.dir/util.cpp.o.d"
+  "CMakeFiles/a.out.dir/visibleedge.cpp.o"
+  "CMakeFiles/a.out.dir/visibleedge.cpp.o.d"
+  "a.out"
+  "a.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
